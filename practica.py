c9216077bcc893e4adb710c96f741ca7f8f126e8
kevinld93 <motorhead2473@gmail.com>
2023-03-29 20:55:11 -0300

    puse un ciclo para saber tabla

--- practica.py
+++ practica.py
@@ -4,3 +4,7 @@ if (palabra=="kevin"):
         print("sos capo")
 else:
         print("sos navo")
+
+num1=int(input("ingrese numero para saber tabla"))
+for i in range(11):
+        print(i,"*",i , i*num1)
